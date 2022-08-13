fa1694f7b1c0ca623fc95bb97071bea20dd7d134
Vittalina <vitalina.alleksandrovna@gmail.com>
2022-08-13 15:03:53 +0300

    commit

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,9 +1,7 @@
 import PropTypes from 'prop-types';
 import { Warning } from 'components/Notification/Notification.styled';
 
-const Notification = ({ message }) => {
-  <Warning>{message}</Warning>;
-};
+const Notification = ({ message }) => <Warning>{message}</Warning>;
 
 Notification.propTypes = {
   message: PropTypes.string,
--- src/components/Notification/Notification.styled.jsx
+++ src/components/Notification/Notification.styled.jsx
@@ -2,5 +2,6 @@ import styled from '@emotion/styled';
 
 export const Warning = styled.p`
   font-family: inherit;
+  font-size: 18px;
   color: red;
 `;
